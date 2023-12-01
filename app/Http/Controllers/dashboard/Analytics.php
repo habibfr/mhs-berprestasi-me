8593229556fd033862d7fbe1ef20ef0a8e1b4d8b
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-01 19:56:37 +0700

    first init

---
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Analytics extends Controller
+{
+  // public function index()
+  // {
+  //   return view('content.dashboard.dashboard');
+  // }
+  public function index()
+  {
+    return view('content.dashboard.dashboards-analytics');
+  }
+}
---
+++ resources/views/content/mahasiswa/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Mahasiswa')
+
+@section('content')
+    <h4 class="py-3 mb-4">
+        Mahasiswa
+    </h4>
+
+@endsection
