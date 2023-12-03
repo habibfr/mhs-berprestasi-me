796cf53a99f73e5a5d7aeb7db7699260703f737c
Excel951 <abigailexcelsisdeo@gmail.com>
2023-12-03 22:40:54 +0700

    ubah navbar, mahasiswa, layout?

--- resources/views/_partials/navbar.blade.php
+++ resources/views/_partials/navbar.blade.php
@@ -1,13 +1,14 @@
-	<div class="container my-3">
+<nav class="mt-3 mb-5">
+	<div class="container">
 		<div class="col-md-12 text-center">
-			<h1>
+			<h1 class="fw-bold" style="position: relative;">
 				KRILIN
+				<div class="position-absolute" style="top: 0; right: 0;">
+					<form action="/logout" method="POST">
+						<button class="btn btn-outline-danger">Logout</button>
+					</form>
+				</div>
 			</h1>
-			<div class="">
-				<form action="/logout" method="POST">
-					<button class="btn btn-outline-danger">Logout</button>
-				</form>
-			</div>
 		</div>
 		<div class="row justify-content-center">
 			<div class="col-md-2 text-center"><a href="/mahasiswa">Mahasiswa</a></div>
@@ -15,4 +16,4 @@
 			<div class="col-md-2 text-center"><a href="/peringkat">Peringkat</a></div>
 		</div>
 	</div>
-
+</nav>
