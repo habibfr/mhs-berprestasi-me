b366c9cf2e7365621ed5c8c2956a4e3e34bf60a6
Excel951 <abigailexcelsisdeo@gmail.com>
2023-12-03 21:56:37 +0700

    tambah layout baru, navbar, dan ubah view mahasiswa

---
+++ resources/views/_partials/navbar.blade.php
@@ -0,0 +1,18 @@
+	<div class="container my-3">
+		<div class="col-md-12 text-center">
+			<h1>
+				KRILIN
+			</h1>
+			<div class="">
+				<form action="/logout" method="POST">
+					<button class="btn btn-outline-danger">Logout</button>
+				</form>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-2 text-center"><a href="/mahasiswa">Mahasiswa</a></div>
+			<div class="col-md-2 text-center"><a href="/kriteria">Kriteria</a></div>
+			<div class="col-md-2 text-center"><a href="/peringkat">Peringkat</a></div>
+		</div>
+	</div>
+
