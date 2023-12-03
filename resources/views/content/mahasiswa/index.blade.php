b596bee2ed603238833e52fab10e54d09c17396c
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-04 00:17:00 +0700

    add filter and modal

--- resources/views/content/mahasiswa/index.blade.php
+++ resources/views/content/mahasiswa/index.blade.php
@@ -71,132 +71,137 @@
 
     <!-- Basic Bootstrap Table -->
     <div class="card">
-        <h5 class="card-header">Table Basic</h5>
+
+        <div class="row">
+            <div class="col">
+                <h5 class="card-header">Table Mahasiswa</h5>
+            </div>
+            <div class="col">
+                <div class="float-end">
+                    <div class="navbar-nav align-items-center card-header">
+                        <div class="nav-item d-flex align-items-center">
+                            <i class="bx bx-search fs-4 lh-0"></i>
+                            <input type="text" class="form-control border-0  shadow-none ps-1 ps-sm-2"
+                                placeholder="Search..." aria-label="Search...">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
         <div class="table-responsive text-nowrap">
             <table class="table">
                 <thead>
                     <tr>
-                        <th>Project</th>
-                        <th>Client</th>
-                        <th>Users</th>
-                        <th>Status</th>
+                        <th>No</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>IPK</th>
+                        <th>SSKM</th>
+                        <th>TOEFL</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody class="table-border-bottom-0">
                     <tr>
-                        <td><i class="bx bxl-angular bx-sm text-danger me-3"></i> <span class="fw-medium">Angular
-                                Project</span></td>
-                        <td>Albert Cook</td>
+                        <td>1</td>
+                        <td>21410100099</td>
+                        <td>Albert Cooking</td>
+                        <td> 3.98 </td>
+                        <td>250</td>
+                        <td>500</td>
                         <td>
-                            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                    <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                                    <img src="{{ asset('assets/img/avatars/6.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Christina Parker">
-                                    <img src="{{ asset('assets/img/avatars/7.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                            </ul>
-                        </td>
-                        <td><span class="badge bg-label-primary me-1">Active</span></td>
-                        <td>
-                            <div class="dropdown">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                    data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i>
-                                        Edit</a>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i>
-                                        Delete</a>
-                                </div>
+                            <div class="inline">
+                                <span class="text-success" data-bs-toggle="modal" data-bs-target="#modalEditMhs"><i
+                                        class="bx bx-edit-alt bx-sm me-2"></i>
+                                </span>
+                                <span class="text-danger" data-bs-toggle="modal" data-bs-target="#modalHapusMhs"><i
+                                        class="bx bx-trash bx-sm me-2"></i>
+                                    </a>
                             </div>
                         </td>
                     </tr>
+
                     <tr>
-                        <td><i class="bx bxl-react bx-sm text-info me-3"></i> <span class="fw-medium">React Project</span>
-                        </td>
-                        <td>Barry Hunter</td>
+                        <td>2</td>
+                        <td>21410100098</td>
+                        <td>Albert Cooking</td>
+                        <td> 3.98 </td>
+                        <td>250</td>
+                        <td>500</td>
                         <td>
-                            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                    <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                                    <img src="{{ asset('assets/img/avatars/6.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Christina Parker">
-                                    <img src="{{ asset('assets/img/avatars/7.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                            </ul>
-                        </td>
-                        <td><span class="badge bg-label-success me-1">Completed</span></td>
-                        <td>
-                            <div class="dropdown">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                    data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="javascript:void(0);"><i
-                                            class="bx bx-edit-alt me-2"></i>
-                                        Edit</a>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-2"></i>
-                                        Delete</a>
-                                </div>
+                            <div class="inline">
+                                <span class="text-success" data-bs-toggle="modal" data-bs-target="#modalEditMhs"><i
+                                        class="bx bx-edit-alt bx-sm me-2"></i>
+                                </span>
+                                <span class="text-danger" data-bs-toggle="modal" data-bs-target="#modalHapusMhs"><i
+                                        class="bx bx-trash bx-sm me-2"></i>
+                                    </a>
                             </div>
                         </td>
                     </tr>
+
                     <tr>
-                        <td><i class="bx bxl-vuejs bx-sm text-success me-3"></i> <span class="fw-medium">VueJs
-                                Project</span></td>
-                        <td>Trevor Baker</td>
+                        <td>3</td>
+                        <td>21410100099</td>
+                        <td>Albert Cooking</td>
+                        <td> 3.98 </td>
+                        <td>250</td>
+                        <td>500</td>
                         <td>
-                            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                    <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                                    <img src="{{ asset('assets/img/avatars/6.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                    class="avatar avatar-xs pull-up" title="Christina Parker">
-                                    <img src="{{ asset('assets/img/avatars/7.png') }}" alt="Avatar"
-                                        class="rounded-circle">
-                                </li>
-                            </ul>
-                        </td>
-                        <td><span class="badge bg-label-info me-1">Scheduled</span></td>
-                        <td>
-                            <div class="dropdown">
-                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                    data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="javascript:void(0);"><i
-                                            class="bx bx-edit-alt me-2"></i> Edit</a>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-2"></i>
-                                        Delete</a>
-                                </div>
+                            <div class="inline">
+                                <span class="text-success" data-bs-toggle="modal" data-bs-target="#modalEditMhs"><i
+                                        class="bx bx-edit-alt bx-sm me-2"></i>
+                                </span>
+                                <span class="text-danger" data-bs-toggle="modal" data-bs-target="#modalHapusMhs"><i
+                                        class="bx bx-trash bx-sm me-2"></i>
+                                    </a>
                             </div>
                         </td>
                     </tr>
+
                     <tr>
+                        <td>4</td>
+                        <td>21410100099</td>
+                        <td>Albert Cooking</td>
+                        <td> 3.98 </td>
+                        <td>250</td>
+                        <td>500</td>
+                        <td>
+                            <div class="inline">
+                                <span class="text-success" data-bs-toggle="modal" data-bs-target="#modalEditMhs"><i
+                                        class="bx bx-edit-alt bx-sm me-2"></i>
+                                </span>
+                                <span class="text-danger" data-bs-toggle="modal" data-bs-target="#modalHapusMhs"><i
+                                        class="bx bx-trash bx-sm me-2"></i>
+                                    </a>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>5</td>
+                        <td>21410100099</td>
+                        <td>Albert Cooking Rotixyzyzyzyz</td>
+                        <td> 3.98 </td>
+                        <td>250</td>
+                        <td>500</td>
+                        <td>
+                            <div class="inline">
+                                <span class="text-success" data-bs-toggle="modal" data-bs-target="#modalEditMhs"><i
+                                        class="bx bx-edit-alt bx-sm me-2"></i>
+                                </span>
+                                <span class="text-danger" data-bs-toggle="modal" data-bs-target="#modalHapusMhs"><i
+                                        class="bx bx-trash bx-sm me-2"></i>
+                                    </a>
+                            </div>
+                        </td>
+                    </tr>
+
+                    {{-- <tr>
                         <td><i class="bx bxl-bootstrap bx-sm text-primary me-3"></i> <span class="fw-medium">Bootstrap
                                 Project</span></td>
                         <td>Jerry Milton</td>
@@ -232,9 +237,71 @@
                                 </div>
                             </div>
                         </td>
-                    </tr>
+                    </tr> --}}
                 </tbody>
             </table>
+
+
+            {{-- modal edit --}}
+            <div class="modal fade" id="modalEditMhs" data-bs-backdrop="static" tabindex="-1" style="display: none;"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel1">Edit Mahasiswa</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col mb-3">
+                                    <label for="nameBasic" class="form-label">Name</label>
+                                    <input type="text" id="nameBasic" class="form-control" placeholder="Enter Name">
+                                </div>
+                            </div>
+                            <div class="row g-2">
+                                <div class="col mb-0">
+                                    <label for="emailBasic" class="form-label">Email</label>
+                                    <input type="email" id="emailBasic" class="form-control"
+                                        placeholder="xxxx@xxx.xx">
+                                </div>
+                                <div class="col mb-0">
+                                    <label for="dobBasic" class="form-label">DOB</label>
+                                    <input type="date" id="dobBasic" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary"
+                                data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-danger">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- modal confirm delete --}}
+            <div class="modal fade" id="modalHapusMhs" data-bs-backdrop="static" tabindex="-1" style="display: none;"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <form class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="backDropModalTitle">Hapus Mahasiswa</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Are you sure to delete this??</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary"
+                                data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-danger">Iya</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         </div>
     </div>
     <!--/ Basic Bootstrap Table -->
