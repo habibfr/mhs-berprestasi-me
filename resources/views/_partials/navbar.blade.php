953bd894d6459a787c20e73374724e1e7ca9cf0e
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-03 23:23:15 +0700

    cahnge typography

--- resources/views/_partials/navbar.blade.php
+++ resources/views/_partials/navbar.blade.php
@@ -1,19 +1,19 @@
 <nav class="mt-3 mb-5">
-	<div class="container">
-		<div class="col-md-12 text-center">
-			<h1 class="fw-bold" style="position: relative;">
-				KRILIN
-				<div class="position-absolute" style="top: 0; right: 0;">
-					<form action="/logout" method="POST">
-						<button class="btn btn-outline-danger">Logout</button>
-					</form>
-				</div>
-			</h1>
-		</div>
-		<div class="row justify-content-center">
-			<div class="col-md-2 text-center"><a href="/mahasiswa">Mahasiswa</a></div>
-			<div class="col-md-2 text-center"><a href="/kriteria">Kriteria</a></div>
-			<div class="col-md-2 text-center"><a href="/peringkat">Peringkat</a></div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="col-md-12 text-center">
+            <h1 class="fw-bold" style="position: relative;">
+                KRILIN
+                <div class="position-absolute" style="top: 0; right: 0;">
+                    <form action="/logout" method="POST">
+                        <button class="btn btn-outline-danger">Logout</button>
+                    </form>
+                </div>
+            </h1>
+        </div>
+        <div class="row justify-content-center">
+            <h3 class="col-md-2 text-center fw-medium"><a href="/mahasiswa">Mahasiswa</a></h3>
+            <h3 class="col-md-2 text-center fw-medium"><a href="/kriteria">Kriteria</a></h3>
+            <h3 class="col-md-2 text-center fw-medium"><a href="/peringkat">Peringkat</a></h3>
+        </div>
+    </div>
 </nav>
