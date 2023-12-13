cf76acf609d1484c7d09dc0e5744a10372f1404d
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-14 02:21:26 +0700

    add kriteria

---
+++ database/seeders/KriteriaSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kriteria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KriteriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $dataKriteria = [
+            [
+                'kriteria' => 'IPK',
+                'atribut' => 'benefit',
+                'bobot'=> 0.3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria' => 'SSKM',
+                'atribut' => 'benefit',
+                'bobot'=> 0.25,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria' => 'TOEFL',
+                'atribut' => 'benefit',
+                'bobot'=> 0.2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria' => 'Karya Tulis',
+                'atribut' => 'benefit',
+                'bobot'=> 0.25,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            
+        ];
+
+        // Insert data dummy ke dalam tabel 'mahasiswa'
+        Kriteria::insert($dataKriteria);
+    }
+}
