b622c983e8c2e3c48160d62fe8300559123e8295
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-18 22:35:26 +0700

    implement login

--- app/Http/Controllers/authentications/LoginBasic.php
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers\authentications;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LoginBasic extends Controller
 {
   public function index()
   {
     return view('content.authentications.auth-login-basic');
+  }
+
+  public function proses_login(Request $request){
+    // dd($request);
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required|min:8'
+    ]);
+
+    $data = [
+      'email' => $request->email,
+      'password' => $request->password
+    ];
+
+    // dd(Auth::attempt($data));
+
+    if(Auth::attempt($data)){
+      return redirect()->route('dashboard');
+    }else{
+      return redirect()->route('login')->with('failed', 'Data tidak valid atau belum terdaftar');
+    }
+  }
+
+  public function logout(){
+    // dd('logout');
+    Auth::logout();
+    return redirect()->route('login')->with('success', "Kamu telah logout, login untuk masuk lagi");
   }
 }
--- resources/views/content/authentications/auth-login-basic.blade.php
+++ resources/views/content/authentications/auth-login-basic.blade.php
@@ -3,69 +3,106 @@
 @section('title', 'Login Basic - Pages')
 
 @section('page-style')
-<!-- Page -->
-<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}">
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}">
 @endsection
 
 @section('content')
-<div class="container-xxl">
-  <div class="authentication-wrapper authentication-basic container-p-y">
-    <div class="authentication-inner">
-      <!-- Register -->
-      <div class="card">
-        <div class="card-body">
-          <!-- Logo -->
-          <div class="app-brand justify-content-center">
-            <a href="{{url('/')}}" class="app-brand-link gap-2">
-              <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-              <span class="app-brand-text demo text-body fw-bold">{{config('variables.templateName')}}</span>
-            </a>
-          </div>
-          <!-- /Logo -->
-          <h4 class="mb-2">Welcome to {{config('variables.templateName')}}! 👋</h4>
-          <p class="mb-4">Please sign-in to your account and start the adventure</p>
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                @if (session('success'))
+                    <div class="alert alert-info alert-dismissible" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
 
-          <form id="formAuthentication" class="mb-3" action="{{url('/')}}" method="GET">
-            <div class="mb-3">
-              <label for="email" class="form-label">Email or Username</label>
-              <input type="text" class="form-control" id="email" name="email-username" placeholder="Enter your email or username" autofocus>
-            </div>
-            <div class="mb-3 form-password-toggle">
-              <div class="d-flex justify-content-between">
-                <label class="form-label" for="password">Password</label>
-                <a href="{{url('auth/forgot-password-basic')}}">
-                  <small>Forgot Password?</small>
-                </a>
-              </div>
-              <div class="input-group input-group-merge">
-                <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-              </div>
-            </div>
-            <div class="mb-3">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="remember-me">
-                <label class="form-check-label" for="remember-me">
-                  Remember Me
-                </label>
-              </div>
-            </div>
-            <div class="mb-3">
-              <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-            </div>
-          </form>
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="{{ url('/') }}" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">@include('_partials.macros', [
+                                    'width' => 25,
+                                    'withbg' => 'var(--bs-primary)',
+                                ])</span>
+                                <span class="app-brand-text demo text-body fw-bold text-title">Briliant</span>
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <h4 class="mb-2">Welcome to Briliant! 👋</h4>
+                        <p class="mb-4">Please sign-in to your account and start the adventure</p>
 
-          <p class="text-center">
-            <span>New on our platform?</span>
-            <a href="{{url('auth/register-basic')}}">
-              <span>Create an account</span>
-            </a>
-          </p>
+                        @if (session('failed'))
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                {{ session('failed') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
+                        @endif
+
+                        <form id="formAuthentication" class="mb-3" action="{{ route('login.proses') }}" method="POST">
+                            @csrf
+                            <div>
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" name="email"
+                                    placeholder="Enter your email" autofocus>
+                            </div>
+                            @if ($errors->has('email'))
+                                <div>
+                                    <small class="text-danger">
+                                        {{ $errors->first('email') }}
+                                    </small>
+                                </div>
+                            @endif
+
+                            <div class="my-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                    <a href="{{ route('forgot.password') }}">
+                                        <small>Forgot Password?</small>
+                                    </a>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                                @if ($errors->has('password'))
+                                    <div>
+                                        <small class="text-danger">
+                                            {{ $errors->first('password') }}
+                                        </small>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="remember-me">
+                                    <label class="form-check-label" for="remember-me">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                            </div>
+                        </form>
+
+                        {{-- <p class="text-center">
+                            <span>New on our platform?</span>
+                            <a href="{{ route('register') }}">
+                                <span>Create an account</span>
+                            </a>
+                        </p> --}}
+                    </div>
+                </div>
+            </div>
+            <!-- /Register -->
         </div>
-      </div>
     </div>
-    <!-- /Register -->
-  </div>
-</div>
-</div>
+    </div>
 @endsection
