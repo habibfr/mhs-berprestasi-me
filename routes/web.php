676eafdaa84710f69174fb464e056cc2bb586b44
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2023-12-18 22:36:30 +0700

    middleware route group

--- routes/web.php
+++ routes/web.php
@@ -49,123 +49,143 @@ use App\Http\Controllers\tables\Basic as TablesBasic;
 
 use App\Http\Controllers\MahasiswaController;
 
-// Main Page Route
-Route::get('/', [Analytics::class, 'index'])->name('dashboard-analytics');
-
-// layout
-Route::get('/layouts/without-menu', [WithoutMenu::class, 'index'])->name('layouts-without-menu');
-Route::get('/layouts/without-navbar', [WithoutNavbar::class, 'index'])->name('layouts-without-navbar');
-Route::get('/layouts/fluid', [Fluid::class, 'index'])->name('layouts-fluid');
-Route::get('/layouts/container', [Container::class, 'index'])->name('layouts-container');
-Route::get('/layouts/blank', [Blank::class, 'index'])->name('layouts-blank');
-
-// pages
-Route::get('/pages/account-settings-account', [AccountSettingsAccount::class, 'index'])->name('pages-account-settings-account');
-Route::get('/pages/account-settings-notifications', [AccountSettingsNotifications::class, 'index'])->name('pages-account-settings-notifications');
-Route::get('/pages/account-settings-connections', [AccountSettingsConnections::class, 'index'])->name('pages-account-settings-connections');
-Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
-Route::get('/pages/misc-under-maintenance', [MiscUnderMaintenance::class, 'index'])->name('pages-misc-under-maintenance');
+Route::get('/', function(){
+  return view('index');
+})->name('homepage');
 
 // authentication
-Route::get('/auth/login-basic', [LoginBasic::class, 'index'])->name('auth-login-basic');
-Route::get('/auth/register-basic', [RegisterBasic::class, 'index'])->name('auth-register-basic');
-Route::get('/auth/forgot-password-basic', [ForgotPasswordBasic::class, 'index'])->name('auth-reset-password-basic');
-
-// cards
-Route::get('/cards/basic', [CardBasic::class, 'index'])->name('cards-basic');
-
-// User Interface
-Route::get('/ui/accordion', [Accordion::class, 'index'])->name('ui-accordion');
-Route::get('/ui/alerts', [Alerts::class, 'index'])->name('ui-alerts');
-Route::get('/ui/badges', [Badges::class, 'index'])->name('ui-badges');
-Route::get('/ui/buttons', [Buttons::class, 'index'])->name('ui-buttons');
-Route::get('/ui/carousel', [Carousel::class, 'index'])->name('ui-carousel');
-Route::get('/ui/collapse', [Collapse::class, 'index'])->name('ui-collapse');
-Route::get('/ui/dropdowns', [Dropdowns::class, 'index'])->name('ui-dropdowns');
-Route::get('/ui/footer', [Footer::class, 'index'])->name('ui-footer');
-Route::get('/ui/list-groups', [ListGroups::class, 'index'])->name('ui-list-groups');
-Route::get('/ui/modals', [Modals::class, 'index'])->name('ui-modals');
-Route::get('/ui/navbar', [Navbar::class, 'index'])->name('ui-navbar');
-Route::get('/ui/offcanvas', [Offcanvas::class, 'index'])->name('ui-offcanvas');
-Route::get('/ui/pagination-breadcrumbs', [PaginationBreadcrumbs::class, 'index'])->name('ui-pagination-breadcrumbs');
-Route::get('/ui/progress', [Progress::class, 'index'])->name('ui-progress');
-Route::get('/ui/spinners', [Spinners::class, 'index'])->name('ui-spinners');
-Route::get('/ui/tabs-pills', [TabsPills::class, 'index'])->name('ui-tabs-pills');
-Route::get('/ui/toasts', [Toasts::class, 'index'])->name('ui-toasts');
-Route::get('/ui/tooltips-popovers', [TooltipsPopovers::class, 'index'])->name('ui-tooltips-popovers');
-Route::get('/ui/typography', [Typography::class, 'index'])->name('ui-typography');
-
-// extended ui
-Route::get('/extended/ui-perfect-scrollbar', [PerfectScrollbar::class, 'index'])->name('extended-ui-perfect-scrollbar');
-Route::get('/extended/ui-text-divider', [TextDivider::class, 'index'])->name('extended-ui-text-divider');
-
-// icons
-Route::get('/icons/boxicons', [Boxicons::class, 'index'])->name('icons-boxicons');
-
-// form elements
-Route::get('/forms/basic-inputs', [BasicInput::class, 'index'])->name('forms-basic-inputs');
-Route::get('/forms/input-groups', [InputGroups::class, 'index'])->name('forms-input-groups');
-
-// form layouts
-Route::get('/form/layouts-vertical', [VerticalForm::class, 'index'])->name('form-layouts-vertical');
-Route::get('/form/layouts-horizontal', [HorizontalForm::class, 'index'])->name('form-layouts-horizontal');
-
-// tables
-Route::get('/tables/basic', [TablesBasic::class, 'index'])->name('tables-basic');
+Route::get('/login', [LoginBasic::class, 'index'])->name('login');
+Route::post('/login-proses', [LoginBasic::class, 'proses_login'])->name('login.proses');
+Route::get('/register', [RegisterBasic::class, 'index'])->name('register');
+Route::get('/forgot-password', [ForgotPasswordBasic::class, 'index'])->name('forgot.password');
 
 
+// Route::group(
+//   ['prefix' => "member", 'as' => 'member.', "middleware" => ['auth:sanctum', 'verified']],
+//   function () {
 
-// Route for Mahasiswa
-Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
+Route::group(['prefix' => 'admin', 'as' => '', 'middleware' => 'auth'], function(){
+  // Main Page Route
+  Route::get('/dashboard', [Analytics::class, 'index'])->name('dashboard');
+  Route::get('/logout', [LoginBasic::class, 'logout'])->name('logout');
 
-// import data for mahasiswa
-Route::post('/mahasiswa/import', [MahasiswaController::class, 'importData'])->name('mahasiswa.import');
-
-// Filter Mahasiswa
-Route::get('/mahasiswa/filter', [MahasiswaController::class, 'filter'])->name('mahasiswa.filter');
-
-// get mahasiswa by id
-Route::get('/mahasiswa/get-mahasiswa/{id}', [MahasiswaController::class, 'getMahasiswaById']);
-
-// update mahasiswa by id
-Route::post('/mahasiswa/update-mahasiswa/{id}', [MahasiswaController::class, 'updateMahasiswa']);
-
-// hapus mahasiswa by id
-Route::post('/mahasiswa/delete/{id}', [MahasiswaController::class, 'destroy']);
+  // layout
+  Route::get('/layouts/without-menu', [WithoutMenu::class, 'index'])->name('layouts-without-menu');
+  Route::get('/layouts/without-navbar', [WithoutNavbar::class, 'index'])->name('layouts-without-navbar');
+  Route::get('/layouts/fluid', [Fluid::class, 'index'])->name('layouts-fluid');
+  Route::get('/layouts/container', [Container::class, 'index'])->name('layouts-container');
+  Route::get('/layouts/blank', [Blank::class, 'index'])->name('layouts-blank');
 
 
-
-// Route for Nilai
-Route::get('/nilai', [NilaiController::class, 'index'])->name('nilai');
-
-// get nilai by id
-Route::get('/nilai/get-nilai/{id}', [NilaiController::class, 'getNilaiByMhsId']);
-
-
-// update mahasiswa by id
-Route::post('/nilai/update-nilai/{id}', [NilaiController::class, 'updateNilai']);
+  // pages
+  Route::get('/pages/account-settings-account', [AccountSettingsAccount::class, 'index'])->name('pages-account-settings-account');
+  Route::get('/pages/account-settings-notifications', [AccountSettingsNotifications::class, 'index'])->name('pages-account-settings-notifications');
+  Route::get('/pages/account-settings-connections', [AccountSettingsConnections::class, 'index'])->name('pages-account-settings-connections');
+  Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
+  Route::get('/pages/misc-under-maintenance', [MiscUnderMaintenance::class, 'index'])->name('pages-misc-under-maintenance');
 
 
-// Route for Peringkat
-// Route::get('/peringkat', [RankingController::class, 'prosesHitung'])->name('peringkat');
-// Route::get('/peringkat', function () {
-//     return view('content.peringkat.index');
-// })
-//         ->name('peringkat');
+  // cards
+  Route::get('/cards/basic', [CardBasic::class, 'index'])->name('cards-basic');
 
-// Route for Kriteria
-Route::get('/kriteria', [KriteriaController::class, 'index'])->name('kriteria');
+  // User Interface
+  Route::get('/ui/accordion', [Accordion::class, 'index'])->name('ui-accordion');
+  Route::get('/ui/alerts', [Alerts::class, 'index'])->name('ui-alerts');
+  Route::get('/ui/badges', [Badges::class, 'index'])->name('ui-badges');
+  Route::get('/ui/buttons', [Buttons::class, 'index'])->name('ui-buttons');
+  Route::get('/ui/carousel', [Carousel::class, 'index'])->name('ui-carousel');
+  Route::get('/ui/collapse', [Collapse::class, 'index'])->name('ui-collapse');
+  Route::get('/ui/dropdowns', [Dropdowns::class, 'index'])->name('ui-dropdowns');
+  Route::get('/ui/footer', [Footer::class, 'index'])->name('ui-footer');
+  Route::get('/ui/list-groups', [ListGroups::class, 'index'])->name('ui-list-groups');
+  Route::get('/ui/modals', [Modals::class, 'index'])->name('ui-modals');
+  Route::get('/ui/navbar', [Navbar::class, 'index'])->name('ui-navbar');
+  Route::get('/ui/offcanvas', [Offcanvas::class, 'index'])->name('ui-offcanvas');
+  Route::get('/ui/pagination-breadcrumbs', [PaginationBreadcrumbs::class, 'index'])->name('ui-pagination-breadcrumbs');
+  Route::get('/ui/progress', [Progress::class, 'index'])->name('ui-progress');
+  Route::get('/ui/spinners', [Spinners::class, 'index'])->name('ui-spinners');
+  Route::get('/ui/tabs-pills', [TabsPills::class, 'index'])->name('ui-tabs-pills');
+  Route::get('/ui/toasts', [Toasts::class, 'index'])->name('ui-toasts');
+  Route::get('/ui/tooltips-popovers', [TooltipsPopovers::class, 'index'])->name('ui-tooltips-popovers');
+  Route::get('/ui/typography', [Typography::class, 'index'])->name('ui-typography');
 
-// get kriteria by id
-Route::get('/kriteria/get-kriteria/{id}', [KriteriaController::class, 'getKriteriaById']);
+  // extended ui
+  Route::get('/extended/ui-perfect-scrollbar', [PerfectScrollbar::class, 'index'])->name('extended-ui-perfect-scrollbar');
+  Route::get('/extended/ui-text-divider', [TextDivider::class, 'index'])->name('extended-ui-text-divider');
 
-// update mahasiswa by id
-Route::post('/kriteria/update-kriteria/{id}', [KriteriaController::class, 'updateKriteria']);
+  // icons
+  Route::get('/icons/boxicons', [Boxicons::class, 'index'])->name('icons-boxicons');
+
+  // form elements
+  Route::get('/forms/basic-inputs', [BasicInput::class, 'index'])->name('forms-basic-inputs');
+  Route::get('/forms/input-groups', [InputGroups::class, 'index'])->name('forms-input-groups');
+
+  // form layouts
+  Route::get('/form/layouts-vertical', [VerticalForm::class, 'index'])->name('form-layouts-vertical');
+  Route::get('/form/layouts-horizontal', [HorizontalForm::class, 'index'])->name('form-layouts-horizontal');
+
+  // tables
+  Route::get('/tables/basic', [TablesBasic::class, 'index'])->name('tables-basic');
 
 
 
+  // Route for Mahasiswa
+  Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa');
 
-// Route for Ranking
-Route::get('/ranking', [RankingController::class, 'index'])->name('ranking');
+  // import data for mahasiswa
+  Route::post('/mahasiswa/import', [MahasiswaController::class, 'importData'])->name('mahasiswa.import');
 
-Route::get('/ranking/hitung', [RankingController::class, 'prosesHitung'])->name('ranking.hitung');
+  // Filter Mahasiswa
+  Route::get('/mahasiswa/filter', [MahasiswaController::class, 'filter'])->name('mahasiswa.filter');
+
+  // get mahasiswa by id
+  Route::get('/mahasiswa/get-mahasiswa/{id}', [MahasiswaController::class, 'getMahasiswaById']);
+
+  // update mahasiswa by id
+  Route::post('/mahasiswa/update-mahasiswa/{id}', [MahasiswaController::class, 'updateMahasiswa']);
+
+  // hapus mahasiswa by id
+  Route::post('/mahasiswa/delete/{id}', [MahasiswaController::class, 'destroy']);
+
+
+
+  // Route for Nilai
+  Route::get('/nilai', [NilaiController::class, 'index'])->name('nilai');
+
+  // get nilai by id
+  Route::get('/nilai/get-nilai/{id}', [NilaiController::class, 'getNilaiByMhsId']);
+
+
+  // update mahasiswa by id
+  Route::post('/nilai/update-nilai/{id}', [NilaiController::class, 'updateNilai']);
+
+
+  // Route for Peringkat
+  // Route::get('/peringkat', [RankingController::class, 'prosesHitung'])->name('peringkat');
+  // Route::get('/peringkat', function () {
+  //     return view('content.peringkat.index');
+  // })
+  //         ->name('peringkat');
+
+  // Route for Kriteria
+  Route::get('/kriteria', [KriteriaController::class, 'index'])->name('kriteria');
+
+  // get kriteria by id
+  Route::get('/kriteria/get-kriteria/{id}', [KriteriaController::class, 'getKriteriaById']);
+
+  // update mahasiswa by id
+  Route::post('/kriteria/update-kriteria/{id}', [KriteriaController::class, 'updateKriteria']);
+
+
+
+
+  // Route for Ranking
+  Route::get('/ranking', [RankingController::class, 'index'])->name('ranking');
+
+  Route::get('/ranking/hitung', [RankingController::class, 'prosesHitung'])->name('ranking.hitung');
+
+
+  //  Route for Peringkat
+  // Route::get('/about', [RankingController::class, 'prosesHitung'])->name('peringkat');
+  Route::get('/about', [MiscUnderMaintenance::class, 'index'])->name('under-maintenance');
+});
